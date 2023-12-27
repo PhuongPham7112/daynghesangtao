edeb358ff073419102431abd0de8a0994ae9cde5
QuanDool <quandool0104@gmail.com>
2023-12-27 06:01:19 -0600

    change about page content

--- daynghesangtao.website/src/routes/about/+page.svelte
+++ daynghesangtao.website/src/routes/about/+page.svelte
@@ -1,45 +1,70 @@
 <script>
-    import Headbar from "../../components/headbar/+headbar.svelte";
+	import Headbar from '../../components/headbar/+headbar.svelte';
 </script>
 
-<div class="bg-cover bg-center p-5 relative flex flex-col items-center" style="background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.75)), url('/assets/about-us-img.webp'); height: 100vh;">
-    <Headbar />
-    <div class="w-1/2 flex-col absolute top-1/2 space-y-5 flex flex-col items-center">
-        <h1 class="font-bt-beausans">Chúng tôi là ai?</h1>
-        <p class="font-bt-beausans">Trung tâm dạy nghề Sáng Tạo được thành lập ngày 7 tháng 11 năm 2015 bởi một nhóm các chuyên gia Tài chính Kế toán giàu kinh nghiệm.</p>
-    </div>
+<div
+	class="bg-cover bg-center p-5 relative flex flex-col items-center"
+	style="background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.75)), url('/assets/about-us-img.webp'); height: 100vh;"
+>
+	<Headbar />
+	<div class="w-1/2 flex-col absolute top-1/2 space-y-5 flex flex-col items-center">
+		<h1 class="font-bt-beausans">Chúng tôi là ai?</h1>
+		<p class="font-bt-beausans">
+			Trung tâm dạy nghề Sáng Tạo được thành lập ngày 7 tháng 11 năm 2015 bởi một nhóm các chuyên
+			gia Tài chính Kế toán giàu kinh nghiệm.
+		</p>
+	</div>
 </div>
 
 <div class="flex items-center justify-center w-full">
-    <div class="md:w-1/3 w-full text-black p-5">
-        <p>
-            Với mục đích dìu dắt các bạn sinh viên năm cuối chuyên ngành Tài chính - Kế toán, Trung tâm xây dựng một chương trình đào tạo bài bản. Học viên được học các kiến thức còn thiếu, bổ sung kiến thức nâng cao, làm việc thực tế và được chỉ đường dẫn lối vào nghề.
-        </p>
-        <br>
-    
-        <p>
-            Thực tế hiện nay kiến thức sinh viên được học trong trường với yêu cầu công việc đầy áp lực trong doanh nghiệp có một khoảng cách không nhỏ. Các bạn chưa hình dung được nghề của mình trong doanh nghiệp diễn ra như thế nào, làm thế nào vượt qua cuộc thi tuyển, làm thế nào để hòa nhập vào môi trường mới. Điều đó làm cho các bạn gặp khó khăn, lúng túng khi thi tuyển và khi làm việc. 
-        </p>
-        <br>
-    
-        <p>
-            Bên cạnh đó một số sinh viên có nguyện vọng học thêm nghề để có nhiều sự lựa chọn công việc, sinh viên học nghề kế toán nhưng muốn làm tài chính, sinh viên học tài chính lại muốn làm kiểm toán.
-        </p>
-        <br>
-    
-        <p>
-            Trung tâm dạy nghề Sáng Tạo với đội ngũ chuyên gia Tài chính Kế toán giàu kinh nghiệm có đầy đủ cơ sở vật chất sẽ là nơi thích hợp để các bạn theo học và thực hiện mơ ước của mình. Kết thúc khóa học các bạn sẽ là người tự tin, bản lĩnh bước vào nghề với niềm tin tất thắng.
-        </p>
-        <br>
-    
-        <div class="text-brightBlue flex items-baseline space-x-2">
-            <svg class="w-[12px] h-[12px] text-brightBlue dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
-                <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
-              </svg>
-              <p>
-                Hãy <a class="underline" href="https://forms.gle/wD8TfsjesNqdebvf7">liên hệ ngay</a> với Trung tâm để được sự tư vấn chi tiết cho bạn.
-              </p>              
-        </div>
-          
-    </div>
+	<div class="md:w-1/3 w-full text-black p-5">
+		<p>
+			Trung tâm dạy nghề Sáng Tạo được thành lập ngày 7 tháng 11 năm 2015 bởi một nhóm các chuyên
+			gia là Kế toán trưởng, Kiểm toán viên cao cấp.
+		</p>
+		<br />
+		<p>
+			Với mục tiêu chúng tôi đặt ra là dìu dắt các bạn sinh viên bước vào nghề Kế toán - Kiểm toán
+			với sự vững vàng về kiến thức, thuần thục về kỹ năng và chuyên nghiệp về thái độ làm việc.
+			Trung tâm đã xây dựng một chương trình đào tạo bài bản và phương pháp huấn luyện đặc biệt để
+			rèn học viên trở thành những con người làm việc chuyên nghiệp.
+		</p>
+		<br />
+		<p>
+			Học viên được hướng dẫn thực hành làm Kế toán viên với sử dụng phần mềm xử lý hồ sơ chứng từ
+			thực tế, tập kê khai báo cáo Thuế theo văn bản quy định của Nhà nước, được truyền dạy kinh
+			nghiệm làm việc trong các loại hình doanh nghiệp và học bổ sung kiến thức còn thiếu.
+		</p>
+		<br />
+
+		<p>
+			Học xong kiến thức chuyên môn Học viên còn được giáo viên đồng hành Tìm việc và Thử việc, được
+			chỉ đường dẫn lối vào nghề và cách hòa nhập với môi trường làm việc mới.
+		</p>
+		<br />
+
+		<p>
+			Trung tâm với đội ngũ chuyên gia đào tạo giàu kinh nghiệm, có trách nhiệm, tâm huyết với nghề
+			sẽ là nơi thích hợp để các bạn theo học và thực hiện mơ ước của mình một cách nhanh nhất.
+		</p>
+		<br />
+
+		<div class="text-brightBlue flex items-baseline space-x-2">
+			<svg
+				class="w-[12px] h-[12px] text-brightBlue dark:text-white"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="currentColor"
+				viewBox="0 0 19 18"
+			>
+				<path
+					d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"
+				/>
+			</svg>
+			<p>
+				Hãy <a class="underline" href="https://forms.gle/wD8TfsjesNqdebvf7">liên hệ ngay</a> với Trung
+				tâm để được sự tư vấn chi tiết cho bạn.
+			</p>
+		</div>
+	</div>
 </div>
