cfb13774b7fdb78eb62767e585ec75894b82c0c8
PhuongPhamUSC <phuongp@usc.edu>
2024-01-01 15:18:51 -0800

    Smaller font for smaller screen sizes

--- daynghesangtao.website/src/components/headbar/+headbar.svelte
+++ daynghesangtao.website/src/components/headbar/+headbar.svelte
@@ -5,11 +5,11 @@
         </div>
         <div class="header-right">
             <ul class="flex justify-around items-center space-x-10">
-                <li><a href="/">Trang chủ</a></li>
-                <li><a href="/about">Giới thiệu</a></li>
-                <li><a href="/courses">Khóa học</a></li>
-                <li><a href="/blog">Kinh Nghiệm</a></li>
-                <li><a href="https://forms.gle/wD8TfsjesNqdebvf7">Liên hệ</a></li>
+                <li><a href="/"><p>Trang chủ</p></a></li>
+                <li><a href="/about"><p>Giới thiệu</p></a></li>
+                <li><a href="/courses"><p>Khóa học</p></a></li>
+                <li><a href="/blog"><p>Kinh Nghiệm</p></a></li>
+                <li><a href="https://forms.gle/wD8TfsjesNqdebvf7"><p>Liên hệ</p></a></li>
             </ul>
         </div>
     </div>
