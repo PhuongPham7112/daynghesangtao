e8367595e7a889cdbf7a30160ddfa5d0593e2e44
QuanDool <quandool0104@gmail.com>
2025-01-13 15:17:05 +0700

    New courses

--- daynghesangtao.website/src/components/courseList/+courseList.svelte
+++ daynghesangtao.website/src/components/courseList/+courseList.svelte
@@ -5,6 +5,7 @@
 <div class="p-5">
     <h2 class="text-deepBlue">Khóa học dạy nghề</h2>
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        <div class="p-4"><Card cardDescription="Miêu tả khóa học: dạy nghề kế toán - kiểm toán với kinh nghiệm thực tế." cardLink={"/courses/ke-toan"} cardTitle="Kế toán" cardImage="assets/rectangle-3.png"/></div>
+        <div class="p-4"><Card cardDescription="Lập bản đồ chỉ đường em đi" cardLink={"/courses/1"} cardTitle="Khóa học 1" cardImage="assets/rectangle-3.png"/></div>
+        <div class="p-4"><Card cardDescription="Chuyên gia dẫn lối vào nghề kế toán-kiểm toán" cardLink={"/courses/2"} cardTitle="Khóa học 2" cardImage="assets/rectangle-3.png"/></div>
     </div>
 </div>
